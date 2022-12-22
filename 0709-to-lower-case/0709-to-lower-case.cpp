9cc2787b2babd1ca2e549876f76312bde5a3e2aa
gbhavya19 <117375195+gbhavya19@users.noreply.github.com>
2022-12-22 17:51:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (9.34%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for (int i=0; i<s.size(); i++){
-            s[i]= tolower(s[i]);
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='A'&&s[i]<='Z'){
+                s[i]-='A'-'a';
+            }
         }
-        return s;
+    return s;
     }
 };
