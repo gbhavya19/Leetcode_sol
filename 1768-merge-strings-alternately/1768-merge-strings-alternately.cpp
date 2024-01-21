d93e0c25c15da40fb726df88009404ddaa65f2e4
Bhavya19 <117375195+gbhavya19@users.noreply.github.com>
2024-01-21 13:52:10 +0530

    Time: 3 ms (49.14%), Space: 7.5 MB (9.15%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -3,16 +3,21 @@ public:
     string mergeAlternately(string word1, string word2) {
         int i=0,j=0;
         string s="";
-        while(i<word1.length() || j<word2.length()){
-        
-        if(i<word1.length())
-            s+=word1[i++];
-           
-         if(j<word2.length())
+        while(i<word1.length() || j<word2.length())
+        {
+            if(i<word1.length())
             
-             s+=word2[j++];
-             }
-    
-    return s;
+                s+=word1[i++];
+                
+            
+        if(j<word2.length())
+        
+            s+=word2[j++];
+                
+        
         }
+        return s;
+        
+        
+    }
 };
